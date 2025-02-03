e9fb84a7a19d4369251be460acf883ca85f00b09
Derek Harnett <DHarnett.dev@proton.me>
2025-02-03 13:34:20 -0800

    add /query route and disambiguation section

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-import express from "express"
-import axios from "axios"
+import express from "express";
+import axios from "axios";
 
-const app = express()
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-const musicBrainzApiUrl = 'https://musicbrainz.org/ws/2'
-const coverArtArchiveApiUrl = 'https://coverartarchive.org'
-
+const musicBrainzApiUrl = "https://musicbrainz.org/ws/2";
+const coverArtArchiveApiUrl = "https://coverartarchive.org";
 
 const queryArtist = (query) => {
-    const url = `${musicBrainzApiUrl}/artist`
-    const params = { query: query }
-    const headers = { 
-        Accept: 'application/json',
-        'User-Agent': 'cover-art-finder/1.0.0 (DHarnett.dev@proton.me)'
-    }
+  const url = `${musicBrainzApiUrl}/artist`;
+  const params = { query: query };
+  const headers = {
+    Accept: "application/json",
+    "User-Agent": "cover-art-finder/1.0.0 (DHarnett.dev@proton.me)",
+  };
 
-    // console.log(url)
-    // console.log(params)
-    // console.log(headers)
+  // console.log(url)
+  // console.log(params)
+  // console.log(headers)
 
-    return axios.get(url, { params: params, headers: headers })
-}
+  return axios.get(url, { params: params, headers: headers });
+};
 
 const parseArtists = (result) => {
-    const artists = []
-    result.data.artists.forEach((artist) => {
-        const nextArtist = {
-            id: artist.id,
-            name: artist.name,
-            score: artist.score,
-            country: artist.country,
-            disambiguation: artist.disambiguation
-        }
-        artists.push(nextArtist)
-    });
-    return artists
-}
+  const artists = [];
+  result.data.artists.forEach((artist) => {
+    const nextArtist = {
+      id: artist.id,
+      name: artist.name,
+      score: artist.score,
+      country: artist.country,
+      disambiguation: artist.disambiguation,
+    };
+    artists.push(nextArtist);
+  });
+  return artists;
+};
 
 // queryArtist('iron maiden')
 //     .then((result) => {
@@ -45,14 +44,13 @@ const parseArtists = (result) => {
 //     )
 //     .catch((error) => {console.log(error)})
 
-
 // const getAlbumIds = (artistID) => {
 //     const url = `${musicBrainzApiUrl}/release`
 //     const params = {
 //         artist: artistID,
 //         type: 'album'
 //     }
-//     const headers = { 
+//     const headers = {
 //         Accept: 'application/json',
 //         'User-Agent': 'cover-art-finder/1.0.0 (DHarnett.dev@proton.me)'
 //      }
@@ -66,26 +64,33 @@ const parseArtists = (result) => {
 //         })
 // }
 
-
 // const getCoverArtUrl = (albumId) => {
 //     const url = `${coverArtArchiveApiUrl}/release/${albumId}`
 // }
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+app.post("/query", (req, res) => {
+  const query = req.body.query;
+  queryArtist(query)
+    .then((result) => {
+      const artists = parseArtists(result);
+      res.render("index.ejs", { query: query, artists: artists });
+      //   console.log(artists);
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+});
 
-app.get('/query', (req, res) => {
-    console.log('/query reached')
+app.post("/disambiguate", (req, res) => {
+  const artistId = body.id;
+  console.log(artistId);
+});
 
-    const body = req.body
-    console.log(body)
-    
-    res.render('index.ejs')
-})
-
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`)
-})
+  console.log(`App running on port ${port}`);
+});
